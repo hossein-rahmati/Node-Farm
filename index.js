f21adcbb77bf72c7d890c65f76be1ef89a02987a
hossein rahmati <rmthossein@gmail.com>
2024-06-18 09:53:28 +0330

    send overview response data

--- index.js
+++ index.js
@@ -22,21 +22,51 @@ const url = require("url");
 // });
 
 ////////////////* SERVER
+function replaceTemplate(temp, product) {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathname = req.url;
 
+  ///overview page
   if (pathname === "/" || pathname === "/overview") {
-    res.end("this is OVERVIEW");
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardHtml);
+
+    res.end(output);
+
+    ///product page
   } else if (pathname === "/product") {
     res.end("this is PRODUCT");
+
+    ///api
   } else if (pathname === "/api") {
     res.writeHead(200, {
       "Content-type": "application/JSON",
     });
     res.end(data);
+
+    ///not found page
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
@@ -45,7 +75,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8080, "127.0.0.1", (err) => {
+server.listen(8000, "127.0.0.1", (err) => {
   if (err) return console.log("An error has occured in listening on port 8080");
-  console.log("Listening to requests on port 8080");
+  console.log("Listening to requests on port 8000");
 });
