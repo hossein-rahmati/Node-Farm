020c93bf1e5b7552ce63512565ccee6c9aec6138
hossein rahmati <rmthossein@gmail.com>
2024-06-20 08:27:39 +0330

    add slugify dependency to app

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+
+const slugify = require("slugify");
+
 const replaceTemplate = require("./modules/replaceTemplate");
 
 ////////////////* FILES
@@ -30,6 +33,8 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
@@ -47,7 +52,6 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-type": "text/html" });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
-    console.log(output);
     res.end(output);
 
     ///api
